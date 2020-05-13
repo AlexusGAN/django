0797742a25a4add4f29a8dead338e4b3c5a9fdd2
AlexusGAN <gubanov-alexey@yandex.ru>
2020-05-13 18:20:36 +0300

    'fs'

--- gan/salo/views.py
+++ gan/salo/views.py
@@ -1,4 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
+import datetime
+import urllib.parse
+import urllib.request
+import re
 
 def salo(request):
-  return render(request, 'salo/salo.html')  
+  url = "http://www.russianchurchlondon.org/calendar/"
+  post = ""
+  try:
+    response = urllib.request.urlopen(url)
+    encoding = response.info().get_param('charset', 'utf8')
+    text = response.read().decode(encoding)
+    re_post = re.compile(r'<span class="headerfast">(.*?)</span>')
+    match = re.search(re_post, text)
+    if (match):
+      post = match[1]
+  except:
+    pass
+
+  return render(request, 'salo/salo.html', {'post':post})  
